c789084286e897141771a78524aaab5e12713835
King Larce Cortes <kingcort01@gmail.com>
2021-12-01 16:58:43 +0800

    build: Add swagger for visualizing expose API

--- src/controllers/deposit-plan.ts
+++ src/controllers/deposit-plan.ts
@@ -1,8 +1,18 @@
+import { Get, Route } from "tsoa";
+
+/**
+ * Expose what's the available interface
+ */
 interface DepositPlanResponse {
   message: string;
 }
 
+@Route("deposit-plan")
 export default class DepositPlanController {
+  /**
+   * Retrieve a sample message from Controller
+   */
+  @Get("/")
   public async getResponse(): Promise<DepositPlanResponse> {
     return {
       message: "Test",
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 
 import Router from "./routes";
 
@@ -10,6 +11,15 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
 app.use(Router);
 
 app.listen(PORT, () => {
