7861fff60e961cdb1b73b2c830ee109494e21471
King Larce Cortes <kingcort01@gmail.com>
2021-12-02 18:10:32 +0800

    fix: Fix ts file discovery issue

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@jest/types";
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
+  roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
